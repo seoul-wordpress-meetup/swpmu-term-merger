d475cb941974fbe76dca7211f871a2d125437042
Changwoo Nam <ep6tri@hotmail.com>
2025-01-19 16:07:51 +0900

    Resolve issue #8 (#12)
    The main file is swm-term-merger.php and index.php is a just mute placeholder.

---
+++ swm-term-merger.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name:       SWM Term Merger
+ * Plugin URI:        https://github.com/seoul-wordpress-meetup/swm-term-merger
+ * Description:       Term merger plugin for WordPress.
+ * Author:            Seoul WordPress Meetup
+ * Author URI:        https://www.meetup.com/ko-KR/wordpress-meetup-seoul/
+ * Requires at least: 5.9.0
+ * Requires PHP:      8.0
+ * Textdomain:        swm-term-merger
+ * License:           GPL-v2-or-later
+ * Version:           0.9.0
+ */
+
+const SWM_TERM_MERGER_MAIN    = __FILE__;
+const SWM_TERM_MERGER_VERSION = '0.9.0';
+
+// Call autoloader earlier than composer autoload.
+require_once __DIR__ . '/inc/functions.php';
+spl_autoload_register('swmTmgrAutoloader');
+
+require_once __dir__ . '/vendor/autoload.php';
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+swmTmgr();
